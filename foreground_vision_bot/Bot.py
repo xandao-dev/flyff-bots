15e32242b021ba56ce4161421996aa71d3572e21
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-10-03 23:28:13 -0300

    fix: move mouse outside game going to far

--- foreground_vision_bot/Bot.py
+++ foreground_vision_bot/Bot.py
@@ -13,7 +13,6 @@ from libs.WindowCapture import WindowCapture
 from libs.ComputerVision import ComputerVision as CV
 
 
-
 @throttle()
 def emit_error(gui_window, msg):
     gui_window.write_event_value("msg_red", msg)
@@ -53,7 +52,7 @@ class Bot:
     def setup(self, window_handler, gui_window):
         self.voice_engine = pyttsx3.init()
         self.window_capture = WindowCapture(window_handler)
-        self.mouse = HumanMouse()
+        self.mouse = HumanMouse(window_handler)
         self.keyboard = HumanKeyboard(window_handler)
         self.all_mobs = MobInfo.get_all_mobs()
         Thread(target=self.__frame_thread, args=(gui_window,), daemon=True).start()
@@ -112,11 +111,14 @@ class Bot:
             try:
                 self.debug_frame, self.frame = self.window_capture.get_screenshot()
             except:
-                emit_error(_throttle_sec=15, gui_window=gui_window, msg="Error getting the frame. Check if window is visible and attach again.")
+                emit_error(
+                    _throttle_sec=15,
+                    gui_window=gui_window,
+                    msg="Error getting the frame. Check if window is visible and attach again.",
+                )
                 sleep(3)
                 continue
 
-            
             if self.config["show_frames"]:
                 if current_mob_info_index >= (len(self.all_mobs) - 1):
                     current_mob_info_index = 0
@@ -184,7 +186,7 @@ class Bot:
         if self.__check_mob_existence():
             self.mouse.left_click(mob_pos)
             self.keyboard.hold_key(VKEY["F1"], press_time=0.06)
-            self.mouse.move_outside_game()
+            self.mouse.move_outside_game(duration=0.2)
             fight_time = time()
             while True:
                 if not self.__check_mob_still_alive(mob_type_cv):
@@ -247,6 +249,7 @@ class Bot:
         return True
 
     """Match Methods"""
+
     def __get_mobs_position(self, mob_name_cv, mob_height_offset, debug=False):
         if mob_name_cv is None or mob_height_offset is None:
             return []
@@ -355,6 +358,6 @@ class Bot:
             self.debug_frame = drawn_frame
 
         if passed_threshold:
-            #print(f"Perin converter found! inventory_perin_converter_match_threshold: {max_val}")
+            # print(f"Perin converter found! inventory_perin_converter_match_threshold: {max_val}")
             return center_loc
-        #print(f"Perin converter not found! inventory_perin_converter_match_threshold: {max_val}")
+        # print(f"Perin converter not found! inventory_perin_converter_match_threshold: {max_val}")
--- foreground_vision_bot/libs/human_mouse/HumanMouse.py
+++ foreground_vision_bot/libs/human_mouse/HumanMouse.py
@@ -1,59 +1,68 @@
 from time import sleep
 from random import uniform, randint
 
-import win32api, win32con
+import win32api, win32con, win32gui
 
 from libs.human_mouse.HumanCurve import HumanCurve
 
 
 class HumanMouse:
-	def __init__(self):
-		self.w = win32api.GetSystemMetrics(0)
-		self.h = win32api.GetSystemMetrics(1)
-		self.outside_points = ((0, 0), (self.w, 0), (0, self.h), (self.w, self.h), 
-							   (round(self.w/2), 0), (0, round(self.h/2)), 
-							   (self.w, round(self.h/2)), (round(self.w/2), self.h))
+    def __init__(self, hwnd):
+        self.hwnd = hwnd
+        self.window_rect = win32gui.GetWindowRect(self.hwnd)
 
-	def move(self, to_point, from_point=None, duration=0.5, human_curve=None):
-		if not from_point:
-			from_point = win32api.GetCursorPos()
-		if not human_curve:
-			human_curve = HumanCurve(from_point, to_point, targetPoints=25)
+    def move(self, to_point, from_point=None, duration=0.5, human_curve=None):
+        if not from_point:
+            from_point = win32api.GetCursorPos()
+        if not human_curve:
+            human_curve = HumanCurve(from_point, to_point, targetPoints=25)
 
-		for point in human_curve.points:
-			win32api.SetCursorPos((int(round(point[0])), int(round(point[1]))))
-			sleep(round(duration / len(human_curve.points), 3))
+        for point in human_curve.points:
+            win32api.SetCursorPos((int(round(point[0])), int(round(point[1]))))
+            sleep(round(duration / len(human_curve.points), 3))
 
-	def move_outside_game(self, from_point=None, duration=0.5, human_curve=None):
-		if not from_point:
-			from_point = win32api.GetCursorPos()
-		if not human_curve:
-			human_curve = HumanCurve(
-				from_point, 
-				self.outside_points[randint(0, len(self.outside_points)-1)], 
-				targetPoints=25
-			)
+    def move_outside_game(self, from_point=None, duration=0.5, human_curve=None):
+        if not from_point:
+            from_point = win32api.GetCursorPos()
+        if not human_curve:
+            human_curve = HumanCurve(
+                from_point, self.__get_random_outside_point(), targetPoints=25
+            )
 
-		for point in human_curve.points:
-			win32api.SetCursorPos((int(round(point[0])), int(round(point[1]))))
-			sleep(round(duration / len(human_curve.points), 3))
+        for point in human_curve.points:
+            win32api.SetCursorPos((int(round(point[0])), int(round(point[1]))))
+            sleep(round(duration / len(human_curve.points), 3))
 
-	def move_like_robot(self, pos, sleep_time=0.05):
-		win32api.SetCursorPos(pos)
-		sleep(sleep_time)
+    def move_like_robot(self, pos, sleep_time=0.05):
+        win32api.SetCursorPos(pos)
+        sleep(sleep_time)
 
-	def right_click(self, pos=None, set_position=False):
-		if set_position and pos:
-			win32api.SetCursorPos(pos)
-			sleep(round(uniform(0.04, 0.07), 4))
-		win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
-		sleep(round(uniform(0.010, 0.025), 4))
-		win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
+    def right_click(self, pos=None, set_position=False):
+        if set_position and pos:
+            win32api.SetCursorPos(pos)
+            sleep(round(uniform(0.04, 0.07), 4))
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
+        sleep(round(uniform(0.010, 0.025), 4))
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
 
-	def left_click(self, pos=None, set_position=False):
-		if set_position and pos:
-			win32api.SetCursorPos(pos)
-			sleep(round(uniform(0.04, 0.07), 4))
-		win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-		sleep(round(uniform(0.010, 0.025), 4))
-		win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    def left_click(self, pos=None, set_position=False):
+        if set_position and pos:
+            win32api.SetCursorPos(pos)
+            sleep(round(uniform(0.04, 0.07), 4))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        sleep(round(uniform(0.010, 0.025), 4))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+    def __get_random_outside_point(self):
+        random_left = (self.window_rect[0], randint(self.window_rect[1], self.window_rect[3]))
+        random_top = (randint(self.window_rect[0], self.window_rect[2]), self.window_rect[1])
+        random_right = (self.window_rect[2], randint(self.window_rect[1], self.window_rect[3]))
+        random_bottom = (randint(self.window_rect[0], self.window_rect[2]), self.window_rect[3])
+
+        outside_points = (
+            random_left,
+            random_top,
+            random_right,
+            random_bottom,
+        )
+        return outside_points[randint(0, len(outside_points) - 1)]
