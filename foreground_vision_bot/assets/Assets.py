8aaff3666c0c906ac77ef380ff9d7ff3a389d86f
JustMonk <night.fire@mail.ru>
2023-03-29 08:40:53 +0500

    moved default mob pack to json storage

--- foreground_vision_bot/assets/Assets.py
+++ foreground_vision_bot/assets/Assets.py
@@ -6,18 +6,6 @@ import os
 import shutil
 from enum import Enum
 
-mob_rosposa_water_path = str(Path(__file__).parent / "names" / "rosposa_water.png")
-mob_kingyo_water_path = str(Path(__file__).parent / "names" / "kingyo_water.png")
-mob_dlakav_water_path = str(Path(__file__).parent / "names" / "dlakav_water.png")
-mob_batto_wind_path = str(Path(__file__).parent / "names" / "batto_wind.png")
-mob_castor_soil_path = str(Path(__file__).parent / "names" / "castor_soil.png")
-mob_kretan_soil_path = str(Path(__file__).parent / "names" / "kretan_soil.png")
-mob_worun_soil_path = str(Path(__file__).parent / "names" / "worun_soil.png")
-mob_cetiri_electricity_path = str(Path(__file__).parent / "names" / "cetiri_electricity.png")
-mob_oseti_electricity_path = str(Path(__file__).parent / "names" / "oseti_electricity.png")
-mob_carvi_fire_path = str(Path(__file__).parent / "names" / "carvi_fire.png")
-mob_nyamuli_fire_path = str(Path(__file__).parent / "names" / "nyamuli_fire.png")
-
 mob_type_wind_path = str(Path(__file__).parent / "mob_types" / "wind.png")
 mob_type_fire_path = str(Path(__file__).parent / "mob_types" / "fire.png")
 mob_type_soil_path = str(Path(__file__).parent / "mob_types" / "soil.png")
@@ -39,87 +27,6 @@ class MobType:
 
 
 class MobInfo:
-    ROSPOSA = {
-        "name": "rosposa",
-        "element": "water",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_rosposa_water_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.WATER,
-    }
-    KINGYO = {
-        "name": "kingyo",
-        "element": "water",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_kingyo_water_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.WATER,
-    }
-    DLAKAV = {
-        "name": "dlakav",
-        "element": "water",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_dlakav_water_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.WATER,
-    }
-    BATTO = {
-        "name": "batto",
-        "element": "wind",
-        "map_name": "Neo Cascada",
-        "height_offset": 125,
-        "name_img": cv.imread(mob_batto_wind_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.WIND,
-    }
-    CASTOR = {
-        "name": "castor",
-        "element": "soil",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_castor_soil_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.SOIL,
-    }
-    KRETAN = {
-        "name": "kretan",
-        "element": "soil",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_kretan_soil_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.SOIL,
-    }
-    WORUN = {
-        "name": "worun",
-        "element": "soil",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_worun_soil_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.SOIL,
-    }
-    OSETI = {
-        "name": "oseti",
-        "element": "electricity",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_oseti_electricity_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.ELECTRICITY,
-    }
-    CARVI = {
-        "name": "carvi",
-        "element": "fire",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_carvi_fire_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.FIRE,
-    }
-    NYAMULI = {
-        "name": "nyamuli",
-        "element": "fire",
-        "map_name": "Neo Cascada",
-        "height_offset": 80,
-        "name_img": cv.imread(mob_nyamuli_fire_path, cv.IMREAD_GRAYSCALE),
-        "element_img": MobType.FIRE,
-    }
-
     @staticmethod
     def add_new_mob(name: str, map_name: str, image_path: str, height_offset: int, element: str) -> None:
         """
@@ -161,7 +68,7 @@ class MobInfo:
         json.dump(new_mobs_list, file)
 
     @staticmethod
-    def get_all_mobs() -> dict:
+    def get_all_mobs() -> dict[str, dict]:
         """
         Get a list of all mobs registered. Using a dump of mobs_list.json file
 
